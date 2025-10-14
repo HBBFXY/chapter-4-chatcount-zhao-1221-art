715e5a62964767e0510b90840ff0707be25d935e
zhao-1221-art <2647525876@qq.com>
2025-10-14 17:58:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# 这个文件用于编写代码
+s = input()
+letter = 0
+digit = 0
+space = 0
+other = 0
+for ch in s:
+  if ch.isalpha():
+    letter += 1
+  elif ch.isdigit():
+    digit += 1
+  elif ch.isspace():
+    space += 1
+  else:
+    other += 1
+print(f'英文字符:{letter}')
+print(f'数字:{digit}')
+print('空格:{space}')
+print(f'其他:{other}')
